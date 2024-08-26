0023b7b2a918413e68cd18701a167630337bc8d4
yunhayeon <yhy5049@naver.com>
2024-08-26 16:58:02 +0900

    feat: 초기 설정 (tailwind)

--- front-app/src/pages/Home.tsx
+++ front-app/src/pages/Home.tsx
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <div>Home 페이지</div>;
+  const pTag = `text-yellow-300`;
+
+  return (
+    <>
+      <p className={pTag}>This is a</p>
+      <p className="bg-color-1">paragraph styled with Tailwind.</p>
+      <div>Home 페이지</div>
+    </>
+  );
 };
 
 export default Home;
--- front-app/tailwind.config.js
+++ front-app/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-1": "#FF0000",
+      },
+    },
   },
   plugins: [],
-}
-
+};
