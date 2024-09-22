e468efd8107e03c06b735a3fbc9e6835b5217160
박보람 <142669102+gittidev@users.noreply.github.com>
2024-09-22 14:00:31 +0900

    feat: containerList 수정

--- front-app/src/components/OverlayPanel.tsx
+++ front-app/src/components/OverlayPanel.tsx
@@ -2,13 +2,16 @@ import { BsExclamationCircleFill } from "react-icons/bs";
 
 const OverlayPanel = () => {
   const panel =
-    "absolute top-0 left-0 right-0 z-40 bg-white opacity-50 flex justify-center items-center h-full";
+    "absolute top-0 left-0 right-0 z-40 flex justify-center items-center h-full";
 
   return (
     <div className={panel}>
-      <div className="flex items-center space-x-2">
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-white opacity-70"></div>
+      <div className="flex items-center space-x-2 z-10">
         <BsExclamationCircleFill />
-        <p className="text-lg text-color-5">로그인이 필요한 서비스입니다.</p>
+        <p className="text-lg text-color-5 font-bold">
+          로그인이 필요한 서비스입니다.
+        </p>
       </div>
     </div>
   );
