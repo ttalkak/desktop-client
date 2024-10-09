c5bc6cff51f2043c3534ad1154a5c1239c52b197
박보람 <142669102+gittidev@users.noreply.github.com>
2024-10-09 10:52:05 +0900

    feat: 로딩 바 수정

--- front-app/src/components/Loading.tsx
+++ front-app/src/components/Loading.tsx
@@ -1,25 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Loading = () => {
   const [position, setPosition] = useState(0);
+  const intervalRef = useRef<number | null>(null); // ref를 사용하여 interval을 저장
 
   useEffect(() => {
     const animate = () => {
-      setPosition((prevPosition) => (prevPosition + 2) % 100);
+      setPosition((prevPosition) =>
+        prevPosition >= 350 ? -350 : prevPosition + 1
+      );
     };
 
-    const intervalId = setInterval(animate, 20);
+    if (!intervalRef.current) {
+      // interval이 설정되지 않았을 때만 설정
+      intervalRef.current = window.setInterval(animate, 15);
+    }
 
-    return () => clearInterval(intervalId);
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
   }, []);
 
   return (
     <div className="w-64 h-2 bg-gray-200 rounded-full overflow-hidden">
       <div
-        className="h-full w-12 bg-gradient-to-r from-transparent via-blue-500 to-transparent"
+        className="h-full w-20 bg-gradient-to-r from-transparent via-color-12 to-transparent"
         style={{
           transform: `translateX(${position}%)`,
-          transition: "transform 0.2s linear",
+          transition: "transform 0.03s linear",
         }}
       />
     </div>
